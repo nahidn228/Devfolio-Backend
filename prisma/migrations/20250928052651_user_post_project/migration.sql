5f4e87c8f49d018102fcf11a86d4b9655e06c549
nahidn228 <nahidn228@gmail.com>
2025-09-28 11:30:52 +0600

    create prisma model and prisma client

---
+++ prisma/migrations/20250928052651_user_post_project/migration.sql
@@ -0,0 +1,65 @@
+-- CreateEnum
+CREATE TYPE "public1"."Role" AS ENUM ('SUPER_ADMIN', 'ADMIN', 'USER');
+
+-- CreateEnum
+CREATE TYPE "public1"."UserStatus" AS ENUM ('ACTIVE', 'INACTIVE', 'BLOCK');
+
+-- CreateTable
+CREATE TABLE "public1"."User" (
+    "id" SERIAL NOT NULL,
+    "name" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "password" TEXT,
+    "role" "public1"."Role" NOT NULL DEFAULT 'USER',
+    "phone" TEXT NOT NULL,
+    "picture" TEXT,
+    "status" "public1"."UserStatus" NOT NULL DEFAULT 'ACTIVE',
+    "isVerified" BOOLEAN NOT NULL DEFAULT false,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "public1"."Post" (
+    "id" SERIAL NOT NULL,
+    "title" TEXT NOT NULL,
+    "content" TEXT NOT NULL,
+    "thumbnail" TEXT,
+    "isFeatured" BOOLEAN NOT NULL DEFAULT false,
+    "tags" TEXT[],
+    "views" INTEGER NOT NULL DEFAULT 0,
+    "authorId" INTEGER NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "Post_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "public1"."Project" (
+    "id" SERIAL NOT NULL,
+    "title" TEXT NOT NULL,
+    "description" TEXT NOT NULL,
+    "thumbnail" TEXT,
+    "liveUrl" TEXT,
+    "repoUrl" TEXT NOT NULL,
+    "techStack" TEXT[],
+    "features" TEXT[],
+    "isFeatured" BOOLEAN NOT NULL DEFAULT false,
+    "authorId" INTEGER NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "Project_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_email_key" ON "public1"."User"("email");
+
+-- AddForeignKey
+ALTER TABLE "public1"."Post" ADD CONSTRAINT "Post_authorId_fkey" FOREIGN KEY ("authorId") REFERENCES "public1"."User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "public1"."Project" ADD CONSTRAINT "Project_authorId_fkey" FOREIGN KEY ("authorId") REFERENCES "public1"."User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
