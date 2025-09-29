6581c2789b1c845341955f60eb6671fb80c01153
nahidn228 <nahidn228@gmail.com>
2025-09-29 11:47:46 +0600

    project api updated

--- src/modules/project/project.route.ts
+++ src/modules/project/project.route.ts
@@ -1,16 +1,12 @@
-import express from 'express';
-import { ProjectController } from './project.controller';
-
+import express from "express";
+import { ProjectController } from "./project.controller";
 
 const router = express.Router();
-router.get("/stats", ProjectController.getProjectStat)
+router.get("/stats", ProjectController.getProjectStat);
 
-router.post(
-    "/",
-    ProjectController.createProject
-)
+router.post("/", ProjectController.createProject);
 
-router.get("/", ProjectController .getAllProjects);
+router.get("/", ProjectController.getAllProjects);
 router.get("/:id", ProjectController.getProjectById);
 router.patch("/:id", ProjectController.updateProject);
 router.delete("/:id", ProjectController.deleteProject);
